950a8574406f3057e4dcac18c2ca64d3b6040ca5
rgathergood <rgathergood@gmail.com>
2018-03-11 16:19:56 +0000

    PHP Test... again

--- website/contact.php
+++ website/contact.php
@@ -1,19 +1,7 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
-
-//Validate first
-if(empty($name)||empty($visitor_email))
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
 
 if(IsInjected($visitor_email))
 {
