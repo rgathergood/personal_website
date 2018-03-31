b0247614d339cdaf427a43711da261e0cd096d0b
rgathergood <rgathergood@gmail.com>
2018-03-31 16:03:38 +0100

    Changed email address

--- website/contact.php
+++ website/contact.php
@@ -14,7 +14,7 @@ $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
 
-$to = "rgathergood@gmail.com";//<== update the email address
+$to = "rgathergoodmail@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
