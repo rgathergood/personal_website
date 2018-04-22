2a964b23fabafb4768b49965d5d87178a8c6012a
rgathergood <rgathergood@gmail.com>
2018-04-22 22:22:41 +0100

    PHP revert

--- website/contact.php
+++ website/contact.php
@@ -2,59 +2,22 @@
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
-
 if(IsInjected($visitor_email))
 {
     echo "Bad email value!";
     exit;
 }
-
-if (empty($_POST["name"])) {
-$errName = "Please enter your name.";
-} else {
-    $name = test_input($_POST["name"]);
-}
-// Check if email is entered
-if (empty($_POST["email"])) {
-$errEmail = "Please enter your email address.";
-} else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is valid format
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $errEmail = "Invalid email format.";
-    }
-}
-
-if (empty($_POST["message"])) {
-$errMessage = "Please enter your message.";
-} else {
-    $message = test_input($_POST["message"]);
-}
-
-// If there are no errors, send the email & output results to the form
-
 $email_from = $visitor_email;//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
-
 $to = "rgathergoodmail@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
-if (!$errName && !$errEmail && !$errPhone &&  !$errMessage && !$errHuman) {
-    if (mail ($to, $subject, $body, $from)) {
-        $result='<div class="alert alert-success"><h2><span class="glyphicon glyphicon-ok"></span> Message sent!</h2><h3>Thank you for contacting us. Someone will be in touch with you soon.</h3></div>';
-    } else {
-        $result='<div class="alert alert-danger"><h2><span class="glyphicon glyphicon-warning-sign"></span> Sorry there was a form processing error.</h2> <h3>Please try again later.</h3></div>';
-       }
-    }
-}
-// mail($to,$email_subject,$email_body,$headers);
-// //done. redirect to thank-you page.
-// header('Location: thank-you.html');
-
-
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
 // Function to validate against any email injection attempts
 function IsInjected($str)
 {
@@ -77,5 +40,4 @@ function IsInjected($str)
     return false;
   }
 }
-
 ?>
